24272179cfc975d0271e5a1f15176dc3266e058b
Elvira2996 <alikova@iksu.kg>
2024-05-15 10:52:22 +0300

    to added section presentation

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,20 +8,22 @@ import Gallery2 from "../../images/gallery2.jpg"
 import Gallery3 from "../../images/gallery3.jpg"
 import Gallery4 from "../../images/gallery4.jpg"
 import Gallery5 from "../../images/gallery5.jpg"
+import Present1 from "../../images/present1.jpg"
+import Present2 from "../../images/present2.jpg"
 
 
 
 function Home() {
 	return (
 		<>
-		
+
 			<section className="about">
-				<div classNameNameName="video-container">
+				<div classNameNameNameName="video-container">
 					<video width="100%" height="auto" controls autoPlay muted loop> <source src={video} type="video/mp4" /></video>
-					<h1 classNameNameName="video-title">La Vacanza 2024</h1>
-					<div classNameNameName="buttons">
-						<button classNameNameName="btn_about">Shop women's</button>
-						<button classNameNameName="btn_about">Discover More</button>
+					<h1 classNameNameNameName="video-title">La Vacanza 2024</h1>
+					<div classNameNameNameName="buttons">
+						<button classNameNameNameName="btn_about">Shop women's</button>
+						<button classNameNameNameName="btn_about">Discover More</button>
 					</div>
 				</div>
 			</section>
@@ -44,36 +46,52 @@ function Home() {
 				</div>
 			</section>
 			<section className="gallery">
-    <div classNameName="gallery-content flex">
-        <div classNameName="gallery-left">
-            <h2>New In</h2>
-        </div>
-        <div className="gallery-right">
-            <div className="gallery-images">
-                <div className="gallery-image">
-                    <img src={Gallery1} alt="Image 1" />
-                    <p className="gallery-text"><a href="#">Athena</a></p>
-                </div>
-				<div className="gallery-image">
-            <img src={Gallery2} alt="Image 2" />
-            <p className="gallery-text"><a href="#">Medusa Buckle</a></p>
-        </div>
-        <div className="gallery-image">
-            <img src={Gallery3} alt="Image 3" />
-            <p className="gallery-text"><a href="#">Medusa '95</a></p>
-        </div>
-        <div className="gallery-image">
-            <img src={Gallery4} alt="Image 4" />
-            <p className="gallery-text"><a href="#">Underwear And Beachwear</a></p>
-        </div>
-        <div className="gallery-image">
-            <img src={Gallery5} alt="Image 5" />
-            <p className="gallery-text"><a href="#">Jewerly</a></p>
-        </div>
-            </div>
-        </div>
-    </div>
-</section>
+				<div className="gallery-content flex">
+					<div className="gallery-left">
+						<h2>New In</h2>
+					</div>
+					<div className="gallery-right">
+						<div className="gallery-images">
+							<div className="gallery-image">
+								<img src={Gallery1} alt="#" />
+								<p className="gallery-text"> <Link to="#">Athena</Link></p>
+							</div>
+							<div className="gallery-image">
+								<img src={Gallery2} alt="#" />
+								<p className="gallery-text"> <Link to="#">Medusa Buckle</Link></p>
+							</div>
+							<div className="gallery-image">
+								<img src={Gallery3} alt="#" />
+								<p className="gallery-text"> <Link to="#">Medusa '95</Link></p>
+							</div>
+							<div className="gallery-image">
+								<img src={Gallery4} alt="#" />
+								<p className="gallery-text"> <Link to="#">Underwear And Beachwear</Link></p>
+							</div>
+							<div className="gallery-image">
+								<img src={Gallery5} alt="#" />
+								<p className="gallery-text"> <Link to="#">Jewerly</Link></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="presentation">
+				<div className="presentation_image">
+					<img src={Present1} alt="#" />
+					<div className="image_content">
+						<h2 className="presentation_title">New In for her</h2>
+						<Link to="#" className="presentation_btn">Shop now</Link>
+					</div>
+				</div>
+				<div className="presentation_image">
+					<img src={Present2} alt="#" />
+					<div className="image_content">
+						<h2 className="presentation_title">Woman's bag</h2>
+						<Link to="#" className="presentation_btn">Shop now</Link>
+					</div>
+				</div>
+			</section>
 		</>
 	);
 }
