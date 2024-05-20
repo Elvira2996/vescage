c9c8ca93980494c82db721b3b5cc617cc9985b91
Elvira2996 <alikova@iksu.kg>
2024-05-20 22:58:00 +0300

    to added section about html

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,51 @@
 import './About.css';
 
-function About() {
+
+function About(){
   return (
     <>
-      
+    
+    <section className="recent">
+    <div className="recent_left">
+        <h2 className="recent_title">RECENT STORIES</h2>
+        <div className="recent_icons">
+            <div className="left_icon"></div>
+            <div className="dots"></div>
+            <div className="dots"></div>
+            <div className="dots"></div>
+            <div className="dots"></div>
+            <div className="dots"></div>
+            <div className="right_icon"></div>
+        </div>
+    </div>
+    <div className="recent_right">
+        <div className="image_container">
+            <img src={recentImage1} alt="#"/>
+            <p className="recent_txt">TEXT 1</p>
+            <Link to="#" className="read_more">Read More</Link>
+        </div>
+        <div className="image_container">
+            <img src={recentImage2} alt="#"/>
+            <p className="recent_txt">TEXT 2</p>
+            <Link to="#" className="read_more">Read More</Link>
+        </div>
+        <div className="image_container">
+            <img src={recentImage3} alt="Image 2"/>
+            <p className="recent_txt">TEXT 2</p>
+            <Link to="#" className="read_more">Read More</Link>
+        </div>
+        <div className="image_container">
+            <img src={recentImage4} alt="#"/>
+            <p className="recent_txt">TEXT 2</p>
+            <Link to="#" className="read_more">Read More</Link>
+        </div>
+        
+    </div>
+</section>
+
     </>
   );
 }
+
 
 export default About;
